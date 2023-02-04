ce3410f02c33bab39c11de87745fb2e25b41e4bc
Alexander Surovtsev <alexander.surovtsev.vrn@gmail.com>
2023-02-04 21:25:39 +0000

    next comit

--- sources/ast.c
+++ sources/ast.c
@@ -1,8 +1,9 @@
 #include "ast.h"
-#include "ast_node.h"
-#include "token.h"
+#include "custom_queue_helpers.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/queue.h>
 
 ast_node_t *tree_head = NULL;
 
@@ -47,7 +48,10 @@ ret_code_t abstract_syntax_tree_evaluate_x(float *result)
 
 static ret_code_t delete_tree(void)
 {
-  custom_queue_t queue = TAILQ_HEAD_INITIALIZER(queue);
+  custom_queue_t        queue = TAILQ_HEAD_INITIALIZER(queue);
+  custom_queue_entry_t *entry;
+  ast_node_t           *node;
+  ret_code_t            ret_code;
 
   if (NULL == tree_head)
   {
@@ -56,14 +60,54 @@ static ret_code_t delete_tree(void)
 
   custom_queue_empty(&queue);
 
+  ret_code = custom_queue_helpers_insert_ast_node(
+      &queue,
+      tree_head,
+      TAIL_OR_LAST);
 
-  while (!TAILQ_EMPTY(&queue))
+  if (RET_CODE_OK == ret_code)
   {
+    free(tree_head);
+    tree_head = NULL;
 
+    while (!TAILQ_EMPTY(&queue))
+    {
+      entry = TAILQ_FIRST(&queue);
+      TAILQ_REMOVE(&queue, entry, entries);
+
+      node = entry->data;
+
+      if (NULL != node->left)
+      {
+        ret_code = custom_queue_helpers_insert_ast_node(
+            &queue,
+            node->left,
+            TAIL_OR_LAST);
+
+        if (RET_CODE_OK != ret_code)
+        {
+          break;
+        }
+      }
+
+      if (NULL != node->right)
+      {
+        ret_code = custom_queue_helpers_insert_ast_node(
+            &queue,
+            node->right,
+            TAIL_OR_LAST);
+        if (RET_CODE_OK != ret_code)
+        {
+          break;
+        }
+      }
+
+      free(node);
+      free(entry);
+    }
   }
 
-
-  return RET_CODE_OK;
+  return ret_code;
 }
 
 
