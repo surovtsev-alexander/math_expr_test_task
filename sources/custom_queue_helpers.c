03813ce2b9be052af0227476fee076a732a9b9ad
Alexander Surovtsev <alexander.surovtsev.vrn@gmail.com>
2023-02-04 20:51:03 +0000

    next commit

--- sources/custom_queue_helpers.c
+++ sources/custom_queue_helpers.c
@@ -1,10 +1,13 @@
 #include "custom_queue_helpers.h"
 
+#include <stdlib.h>
+
 ret_code_t custom_queue_helper_insert_token(
     custom_queue_t *queue,
     const token_t  *token,
     bool            head_not_tail)
 {
+  ret_code_t ret_code;
   token_t *token_to_store;
 
   if (NULL == token)
@@ -18,9 +21,16 @@ ret_code_t custom_queue_helper_insert_token(
     return RET_CODE_NO_MEMORY;
   }
   
-  return custom_queue_insert(
+  ret_code = custom_queue_insert(
       queue,
       token_to_store,
       head_not_tail);
+
+  if (RET_CODE_OK != ret_code)
+  {
+    free(token_to_store);
+  }
+
+  return ret_code;
 }
 
