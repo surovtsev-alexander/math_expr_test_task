f97707d3194097f74292f1275f6ed279708d8043
Alexander Surovtsev <alexander.surovtsev.vrn@gmail.com>
2023-02-04 17:21:37 +0000

    next commit

--- sources/stream_tokenizer.c
+++ sources/stream_tokenizer.c
@@ -10,9 +10,12 @@ void stream_tokenizer_next_token(
     FILE        *input_stream,
     token_t     *token)
 {
-  int                            symbol;
-  token_id_t                     token_id;
-  float                          number;
+  int        symbol;
+  token_id_t token_id;
+  float      number;
+  bool       float_number_after_point_symbol;
+  float      after_point_multiplier;
+  int        x;
 
   if (EOF != stored_symbol)
   {
@@ -36,13 +39,40 @@ void stream_tokenizer_next_token(
   {
     number   = symbol - '0';
 
-    for (
-        symbol = getc(input_stream);
-        isdigit(symbol);
-        symbol = getc(input_stream))
+    float_number_after_point_symbol = false;
+    after_point_multiplier = 1;
+    for (;;)
     {
-      number *= 10;
-      number += symbol - '0';
+      symbol = getc(input_stream);
+
+      if ('.' == symbol || ',' == symbol)
+      {
+        if (float_number_after_point_symbol)
+        {
+          token_id = TOKEN_IDS;
+          break;
+        }
+        float_number_after_point_symbol = true;
+        continue;
+      }
+
+      if (isdigit(symbol))
+      {
+        x = symbol - '0';
+        if (!float_number_after_point_symbol)
+        {
+          number *= 10;
+          number += x;
+        }
+        else
+        {
+          after_point_multiplier /= 10;
+          number += after_point_multiplier * x;
+        }
+        continue;
+      }
+
+      break;
     }
     stored_symbol = symbol;
   }
