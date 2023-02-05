b9efbe75a6d4b8f7a4cbf89978ddfa503b98a5d8
Alexander Surovtsev <alexander.surovtsev.vrn@gmail.com>
2023-02-05 01:22:46 +0000

    add ast_folder

---
+++ include/ast_folder.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "ret_code.h"
+#include "ast_node.h"
+
+ret_code_t ast_folder_fold(ast_node_t **root);
+
---
+++ sources/ast_folder.c
@@ -0,0 +1,13 @@
+#include "ast_folder.h"
+
+ret_code_t ast_folder_fold(ast_node_t **root)
+{
+  ret_code_t ret_code;
+
+
+  ret_code = RET_CODE_OK;
+
+
+  return ret_code;
+}
+
