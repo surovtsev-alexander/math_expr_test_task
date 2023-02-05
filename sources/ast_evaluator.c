c970ef5c33c576a981186bfd24df52d440c42566
Alexander Surovtsev <alexander.surovtsev.vrn@gmail.com>
2023-02-05 10:18:33 +0000

    next commit

--- sources/ast_evaluator.c
+++ sources/ast_evaluator.c
@@ -6,7 +6,7 @@ ret_code_t ast_evaluator_evaluate(ast_node_t *root)
 {
   ret_code_t             ret_code;
   token_t               *root_token;
-  token_id_t             root_token_id;
+  token_t               *left_child_token;
 
 
   if (NULL == root)
@@ -18,32 +18,30 @@ ret_code_t ast_evaluator_evaluate(ast_node_t *root)
 
   root_token = &(root->token);
 
+  if (!token_id_is_number(root->right->token.token_id))
+  {
+    ret_code = RET_CODE_UNEXPECTED_TOKEN;
+  }
+  if (!token_id_is_equal_sign(root->token.token_id))
+  {
+    ret_code == RET_CODE_UNEXPECTED_TOKEN;
+  }
+
   while (RET_CODE_OK == ret_code)
   {
-    root_token_id = root_token->token_id;
-    if (token_id_is_number(root_token_id))
+    left_child_token = &(root->left->token);
+    if (token_id_is_x(left_child_token->token_id))
     {
       break;
     }
-    else if (token_id_is_equal_sign(root_token_id))
+    else if (token_id_is_change_sign(left_child_token->token_id))
     {
-      if (token_id_is_x(root->left->token.token_id))
-      {
-        if (!token_id_is_number(root->right->token.token_id))
-        {
-          ret_code = RET_CODE_UNEXPECTED_TOKEN;
-        }
-        break;
-      }
+      ret_code = RET_CODE_UNEXPECTED_TOKEN;
 
-      ret_code = RET_CODE_UNEXPECTED_TOKEN;
       break;
     }
-    else
-    {
-      ret_code = RET_CODE_UNEXPECTED_TOKEN;
-      break;
-    }
+
+    ret_code = RET_CODE_UNEXPECTED_TOKEN;
   }
 
   return ret_code;
